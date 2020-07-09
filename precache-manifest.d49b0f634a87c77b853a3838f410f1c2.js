cfb965d085731021b6519ef518b4d205ad68d1b3
mowgli77 <bartend.it13@gmail.com>
2020-07-09 21:32:46 +0300

    Updates

---
+++ precache-manifest.d49b0f634a87c77b853a3838f410f1c2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b942f109fc667d24de5cabb877f2504",
+    "url": "/star-wars-films/index.html"
+  },
+  {
+    "revision": "244dfce5a0a54b372724",
+    "url": "/star-wars-films/static/css/main.20dc33bc.chunk.css"
+  },
+  {
+    "revision": "cd5e3e54413e9da35c92",
+    "url": "/star-wars-films/static/js/2.6d13af39.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/star-wars-films/static/js/2.6d13af39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "244dfce5a0a54b372724",
+    "url": "/star-wars-films/static/js/main.63435f37.chunk.js"
+  },
+  {
+    "revision": "c769b841fd2121d440ef",
+    "url": "/star-wars-films/static/js/runtime-main.24e24bb8.js"
+  },
+  {
+    "revision": "4c86c3df48e43c25b542db0fd4e3e6dd",
+    "url": "/star-wars-films/static/media/episode1.4c86c3df.jpg"
+  },
+  {
+    "revision": "74375f3f5f2a1984cd6c6f9c1e64ae1e",
+    "url": "/star-wars-films/static/media/episode2.74375f3f.jpg"
+  },
+  {
+    "revision": "5256d4c3bf6fcf193bd84697d8baee2f",
+    "url": "/star-wars-films/static/media/episode3.5256d4c3.jpg"
+  },
+  {
+    "revision": "962906945366d3c09f56acf24a5722a7",
+    "url": "/star-wars-films/static/media/episode4.96290694.jpg"
+  },
+  {
+    "revision": "4fd8a9ff68fa5450f9201c72e1adcf9e",
+    "url": "/star-wars-films/static/media/episode5.4fd8a9ff.jpg"
+  },
+  {
+    "revision": "eea133cb535ac139fa4e12ad74d9c1f0",
+    "url": "/star-wars-films/static/media/episode6.eea133cb.jpg"
+  }
+]);
