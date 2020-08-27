bb505d27492c121fe4714c3ce9342338b7c72e5f
mowgli77 <bartend.it13@gmail.com>
2020-08-27 13:40:29 +0300

    some else fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,4 @@
 import React, {useState} from 'react';
-import {getFilmsThunk, sortFilmsByZYX} from "../redux/actions";
-
 
 const Header = (props) => {
 
@@ -31,9 +29,9 @@ const Header = (props) => {
             <header className="header">
                 <div className={"header__search"}>
                     <input placeholder={'Search film'} type={'text'} onKeyUp={props.searchText} />
-                    <button onClick={sortByABC} className={`header__button ${ABCSortisActive && 'header__button_active'}`}>Sort by ABC</button>
-                    <button onClick={sortByZYX} className={`header__button ${ZYXSortisActive && 'header__button_active'}`}>Sort by ZYX</button>
-                    <button onClick={dontSort} className={`header__button ${dontSortIsActive && 'header__button_active'}`}>Don`t sort</button>
+                    <button onClick={sortByABC} disabled={ABCSortisActive} className={`header__button`}>Sort by ABC</button>
+                    <button onClick={sortByZYX} disabled={ZYXSortisActive} className={`header__button`}>Sort by ZYX</button>
+                    <button onClick={dontSort} disabled={dontSortIsActive} className={`header__button`}>Don`t sort</button>
                 </div>
             </header>
     );
